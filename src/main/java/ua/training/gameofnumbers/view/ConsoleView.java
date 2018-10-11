db6c7954b6f9531acc541c50d8f449b98fffedb4
Javaphite <javaphite@gmail.com>
2018-10-11 13:03:52 +0300

    Add simple view class for console app - ConsoleView

---
+++ src/main/java/ua/training/gameofnumbers/view/ConsoleView.java
@@ -0,0 +1,17 @@
+package ua.training.gameofnumbers.view;
+
+/**
+ * Basic view intended to display messages on standard output.
+ */
+
+public class ConsoleView {
+
+    /**
+     * Displays string message as separate line using standard output.
+     * @param message to be displayed;
+     */
+    public void displayMessage(String message) {
+        System.out.println(message);
+    }
+
+}
