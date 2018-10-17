6ed4b568303347cf95f02e30131c7ba2bcd7957f
Javaphite <javaphite@gmail.com>
2018-10-18 01:50:48 +0300

    Console view logic fully updated to work with new resource bundles.

--- src/main/java/ua/training/contactbook/view/ConsoleView.java
+++ src/main/java/ua/training/contactbook/view/ConsoleView.java
@@ -1,17 +1,55 @@
 package ua.training.contactbook.view;
 
+import ua.training.contactbook.controllers.BaseController;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 /**
  * Basic view intended to display messages on standard output.
  */
 
 public class ConsoleView {
 
+    private static final String MESSAGES_BUNDLE_NAME = "messages";
+
+    public static final ResourceBundle BUNDLE =
+            ResourceBundle.getBundle(MESSAGES_BUNDLE_NAME, Locale.getDefault());
+
     /**
      * Displays string message as separate line using standard output.
-     * @param message  object that encapsulates message to be displayed;
+     * @param object object that encapsulates message to be displayed;
      */
-    public void displayMessage(Object message) {
-        System.out.println(message);
+    public void displayMessage(Object object) {
+        System.out.println(object);
+    }
+
+    public void displayMessage(Message message) {
+        if (BUNDLE.containsKey(message.toString())) {
+            displayMessage(BUNDLE.getString(message.toString()));
+        }
+    }
+
+    public void displayMessage(Message messageTemplate, String field) {
+        displayMessage(buildMessage(messageTemplate, field, ""));
+    }
+
+    public void displayMessage(Message messageTemplate, String field, String hint) {
+        displayMessage(buildMessage(messageTemplate, field, hint));
+    }
+
+    private String buildMessage(Message messageTemplate, String field, String hint) {
+        String message = BUNDLE.getString(messageTemplate.toString());
+
+        if (BUNDLE.containsKey("placeholders.field")) {
+           message = message.replaceAll(BUNDLE.getString("placeholders.field"), field);
+        }
+
+        if (BUNDLE.containsKey("placeholders.hint")) {
+           message = message.replaceAll(BUNDLE.getString("placeholders.hint"), hint);
+        }
+
+        return message;
     }
 
 }
---
+++ src/main/java/ua/training/contactbook/view/Message.java
@@ -0,0 +1,20 @@
+package ua.training.contactbook.view;
+
+public enum Message {
+    INTRODUCTION("info.introduction"),
+    NEW_CONTACT("info.new_contact"),
+    INCORRECT_INPUT("warn.incorrect_input"),
+    INPUT("info.demand_input"),
+    SHOW_CONTACT("info.show_contact");
+
+    Message(String text) {
+        this.text = text;
+    }
+
+    private final String text;
+
+    @Override
+    public String toString() {
+        return text;
+    }
+}
